158fde97e3660ed92264624fc022674c8b1931e0
ksascomm <ksasweb@jhu.edu>
2015-05-19 16:41:24 -0400

    style and homepage updates

--- web/app/themes/timmygelles-theme/front-page.php
+++ web/app/themes/timmygelles-theme/front-page.php
@@ -1,7 +1,8 @@
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
   <span id="section1"></span>
   <?php get_template_part('templates/content', 'page'); ?>
   <span id="section2"></span>
+  <div class="container">
   <?php get_template_part('templates/content', 'portfolio'); ?>
+  </div>
 <?php endwhile; ?>
--- web/app/themes/timmygelles-theme/templates/content-page.php
+++ web/app/themes/timmygelles-theme/templates/content-page.php
@@ -1,8 +1,16 @@
 <section class="content">
+<div class="homepage-text">
+<div class="container">
+<div class="homepage-text-inner">
 <?php the_content(); ?>
+</div>
+</div>
+</div>
+<div class="container">
 <?php if ( get_post_meta($post->ID, 'ecpt_callout', true) ) : ?><div class="callout"><?php echo get_post_meta($post->ID, 'ecpt_callout', true);?></div>		
 <?php endif;?>
 <?php if ( get_post_meta($post->ID, 'ecpt_callout2', true) ) : ?><div class="callout"><?php echo get_post_meta($post->ID, 'ecpt_callout2', true);?></div>						
 <?php endif;?>
+</div>
 </section>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
