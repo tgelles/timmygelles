c8608141669c6fbfcec5ea742c5ac822877abfc9
tgelles <tgelles@pha.jhu.edu>
2015-05-27 18:05:10 -0400

    removed ksas theme & readded my theme to composer file

--- web/app/themes/ksas_flagship_blog/templates/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="content-info" role="contentinfo">
-<?php if( is_home()) : ?>
- <div class="container">
-    <?php dynamic_sidebar('sidebar-footer-1'); ?>
-    <?php dynamic_sidebar('sidebar-footer-2'); ?>
-    <?php dynamic_sidebar('sidebar-footer-3'); ?>
-  </div> 
-<?php endif; ?>
-</footer>
