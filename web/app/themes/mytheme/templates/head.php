5390f46e27ad936dfc92cd4cd7edcfd0ef62b1bc
ksascomm <ksasweb@jhu.edu>
2015-06-09 13:47:33 -0400

    edited GA tracking code & location

--- web/app/themes/mytheme/templates/head.php
+++ web/app/themes/mytheme/templates/head.php
@@ -5,4 +5,14 @@
   <link href='http://fonts.googleapis.com/css?family=Neuton:300,700|Montserrat' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <?php wp_head(); ?>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40558845-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
