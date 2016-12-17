e2a344fa28ab769bcf36ad403e90737832860614
tgelles <timothygelles@gmail.com>
2016-12-17 16:03:13 -0500

    schema markup

--- web/app/themes/timmygelles-theme/templates/content-portfolio.php
+++ web/app/themes/timmygelles-theme/templates/content-portfolio.php
@@ -12,26 +12,28 @@
 <?php if($portfolio_query->have_posts()): global $more; ?>
   <div class="row">
     <?php while($portfolio_query->have_posts()): $portfolio_query->the_post(); $more = 0; ?>
-    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-lg-height col-md-height col-sm-height col-top portfolio">
+    <article class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-lg-height col-md-height col-sm-height col-top portfolio"  itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
       <div class="portfolio-item text-center">
-        <h3>
+        <h3 itemprop="headline">
           <?php if (get_post_meta($post->ID, 'ecpt_portfolio_title', true)) : ?>
             <?php echo get_post_meta($post->ID, 'ecpt_portfolio_title', true); ?>
           <?php endif; ?>
         </h3>
 
         <?php $link = get_post_meta($post->ID, 'ecpt_portfolio_link', true);
-            if( $link ) { ?>
-              <a href="<?php echo $link; ?>"><?php the_post_thumbnail('full', array('class' => 'portfolio-img img-responsive') ); ?></a>
-            <?php } else {
-              the_post_thumbnail('full', array('class' => 'portfolio-img img-responsive') );
-        } ?>
+            if( $link ) : ?>
+            <div itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+              <a href="<?php echo $link; ?>"><?php the_post_thumbnail('full', array('class' => 'portfolio-img img-responsive', 'itemprop' => 'image') ); ?></a>
+            <?php else : ?>
+              <?php the_post_thumbnail('full', array('class' => 'portfolio-img img-responsive', 'itemprop' => 'image') ); ?>
+            </div>
+            <?php endif; ?>
 
         <button class="btn btn-primary btn-portfolio" type="button" data-toggle="collapse" data-target="#post-<?php echo get_the_ID(); ?>" aria-expanded="false" aria-controls="collapseExample">
           Tell Me More!
         </button>
 
-        <div class="collapse" id="post-<?php echo get_the_ID(); ?>">
+        <div class="collapse" id="post-<?php echo get_the_ID(); ?>" itemprop="text">
           <ul class="list-group list-group-flush text-center">
             <?php if (get_post_meta($post->ID, 'ecpt_portfolio_role', true)) : ?>
               <li class="list-group-item"><strong>Role:</strong><br><?php echo get_post_meta($post->ID, 'ecpt_portfolio_role', true); ?></li>
@@ -46,11 +48,12 @@
             <?php endif; ?>
             </ul>
         </div>
-  </div>
-</div> <!--end: portfolio item -->
-        <?php if ( $i%2 == 0 ){ echo '<div class="clearfix visible-sm-block">'; } // show clearfix after each 2 cols for xs ?>
-        <?php if ( $i%3 == 0 ){ echo '<div class="clearfix visible-lg-block visible-md-block">'; } // show clearfix after each 3 cols for lg and md ?>
+      </div>
+    </article> <!--end: portfolio item -->
 
-      <?php endwhile; ?>
-</div><!-- end: row -->
-<?php endif; ?>
+    <?php if ( $i%2 == 0 ){ echo '<div class="clearfix visible-sm-block">'; } // show clearfix after each 2 cols for xs ?>
+    <?php if ( $i%3 == 0 ){ echo '<div class="clearfix visible-lg-block visible-md-block">'; } // show clearfix after each 3 cols for lg and md ?>
+
+  <?php endwhile; ?>
+  </div><!-- end: row -->
+  <?php endif; ?>
--- web/app/themes/timmygelles-theme/templates/header.php
+++ web/app/themes/timmygelles-theme/templates/header.php
@@ -1,4 +1,4 @@
-<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
+<header class="banner navbar navbar-default navbar-fixed-top" itemscope="itemscope" itemtype="http://schema.org/WPHeader" role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
@@ -7,10 +7,10 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>" itemprop="headline"><?php bloginfo('name'); ?></a>
     </div>
  
-    <nav class="collapse navbar-collapse" role="navigation">
+    <nav class="collapse navbar-collapse" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav navbar-right']);
